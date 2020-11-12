d9eb06d46a27db58cfa2b56cc9a871203a81ab68
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-11-11 18:00:53 -0600

    Update sailor_race.js

--- sailor_race.js
+++ sailor_race.js
@@ -1,2 +1,14 @@
 "use strict";
-window.alert(“I love miso”);
+
+  var canvas = document.getElementById("canvas");
+  canvas.width = canvas.scrollWidth;
+  canvas.height = canvas.scrollHeight;
+  var cx = canvas.getContext('2d');
+  var square = {x:0,y:0, width:100, height:100}
+  function draw() {
+    cx.clearRect(0,0,canvas.width,canvas.height);
+    square.x += 1;
+    cx.fillRect(square.x, square.y, square.width, square.height)
+    window.requestAnimationFrame(draw);
+  }
+  draw();
