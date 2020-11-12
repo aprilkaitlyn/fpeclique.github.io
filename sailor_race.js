3b96e6f8b2f6e35d2c380dc0d4772db947dbedcf
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-11-11 18:37:28 -0600

    Update sailor_race.js

--- sailor_race.js
+++ sailor_race.js
@@ -1,6 +1,7 @@
 "use strict";
 
-document.getElementById("light").addEventListener("click",changeLight);
+var clicky = document.getElementById("light");
+clicky.addEventListener('click',changeLight);
 windows.onload = function changeLight(e) {
    if (document.getElementById("light").src == "lights_red.png") 
         {
