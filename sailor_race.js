65778332884393b216f2dd363bc70a331d2cec4f
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-11-14 00:58:57 -0600

    Update and rename testtest.js to sailor_race.js

---
+++ sailor_race.js
@@ -0,0 +1,98 @@
+"use strict";
+
+function changeLight(e) {
+   document.getElementById("light").src = "lights_green.png"; } //make light green, onclick event
+  
+//no # -> sailor moon, 2 -> jupiter, 3 -> mars, 4 -> mercury, 5 -> venus
+
+function myMove(event) { //start the race, onclick event
+  //create variable for each guardian's icon
+  var elem = document.getElementById("animate");
+  var elem2 = document.getElementById("animate2");
+  var elem3 = document.getElementById("animate3");
+  var elem4 = document.getElementById("animate4");
+  var elem5 = document.getElementById("animate5");
+
+  //create variable for each guardian's horizontal position
+  var pos = 0;
+  var pos2 = 0;
+  var pos3 = 0;
+  var pos4 = 0;
+  var pos5 = 0;
+  
+  var speed = Math.floor((Math.random() * 10) + 5);
+  var speed2 = Math.floor((Math.random() * 10) + 5);
+  var speed3 = Math.floor((Math.random() * 10) + 5);
+  var speed4 = Math.floor((Math.random() * 10) + 5);
+  var speed5 = Math.floor((Math.random() * 10) + 5);
+  
+  //start movement function for each guardian, depending on their random speed
+  var id = setInterval(frame, speed);
+  var id2 = setInterval(frame2, speed2);
+  var id3 = setInterval(frame3, speed3);
+  var id4 = setInterval(frame4, speed4);
+  var id5 = setInterval(frame5, speed5);
+  
+  var end = (window.innerWidth - 100); //image is 100px -> end race at end of screen - 100px
+   
+function frame() { //sailor moon
+    if (pos == end) {
+       clearInterval(id);
+       clearInterval(id2);
+       clearInterval(id3); //when someone reaches the end, stop the race
+       clearInterval(id4);
+       clearInterval(id5);
+       document.getElementById("winner").src = "sailormoonwins.png"; } //display winner's picture by setting image source
+    else {
+      pos++; //continue race
+      elem.style.left = pos + 'px'; } //move guardian icon to the right
+}
+function frame2() { //sailor jupiter
+    if (pos2 == end) {
+       clearInterval(id);
+       clearInterval(id2);
+       clearInterval(id3);
+       clearInterval(id4);
+       clearInterval(id5);
+       document.getElementById("winner").src = "sailorjupiter.jpg"; } 
+   else {
+      pos2++;
+      elem2.style.left = pos2 + 'px'; }
+}
+function frame3() { //sailor mars
+    if (pos3 == end) {
+      clearInterval(id);
+       clearInterval(id2);
+       clearInterval(id3);
+       clearInterval(id4);
+       clearInterval(id5);
+       document.getElementById("winner").src = "sailormars.png"; }
+    else {
+      pos3++;
+      elem3.style.left = pos3 + 'px'; }
+}
+function frame4() { //sailor mercury
+    if (pos4 == end) {
+      clearInterval(id);
+       clearInterval(id2);
+       clearInterval(id3);
+       clearInterval(id4);
+       clearInterval(id5);
+       document.getElementById("winner").src = "sailormercury.png"; }
+    else {
+      pos4++;
+      elem4.style.left = pos4 + 'px'; }
+}
+function frame5() { //sailor venus
+    if (pos5 == end) {
+      clearInterval(id);
+       clearInterval(id2);
+       clearInterval(id3);
+       clearInterval(id4);
+       clearInterval(id5);
+       document.getElementById("winner").src = "sailorvenus.png"; }
+    else {
+      pos5++;
+      elem5.style.left = pos5 + 'px'; }
+ }
+}
