7a86c733ab95508f4a722ff4fc2840e06140f64d
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-11-13 22:08:00 -0600

    Update sailor_race.js

--- sailor_race.js
+++ sailor_race.js
@@ -1,14 +1,81 @@
 "use strict";
 
-var clicky = document.getElementById("light");
-clicky.addEventListener('click',changeLight);
-windows.onload = function changeLight(e) {
-   if (document.getElementById("light").src == "lights_red.png") 
-        {
-            document.getElementById("light").src = "lights_green.png";
-        }
-        else 
-        {
-            document.getElementById("light").src = "lights_red.png";
-        }
+function changeLight(e) {
+   document.getElementById("light").src = "lights_green.png"; 
+}
+  
+  function myMove() {
+  var elem = document.getElementById("animate");
+  var pos = 0;
+  var id = setInterval(frame, Math.floor((Math.random() * 9) + 1));
+  var end = (window.innerWidth - 100);
+  function frame() {
+    if (pos == end) {
+      clearInterval(id);
+      document.getElementById("winner").src = "sailormoonwins.png"; 
+    } else {
+      pos++;
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+  function myMove2() {
+  var elem = document.getElementById("animate2");
+  var pos = 0;
+  var id = setInterval(frame, Math.floor((Math.random() * 11) + 1));
+  var end = (window.innerWidth - 100);
+  function frame() {
+    if (pos == end) {
+      clearInterval(id);
+      document.getElementById("winner").src = "sailorjupiter.png";
+    } else {
+      pos++;
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+    function myMove3() {
+  var elem = document.getElementById("animate3");
+  var pos = 0;
+  var id = setInterval(frame, Math.floor((Math.random() * 10) + 1));
+  var end = (window.innerWidth - 100);
+  function frame() {
+    if (pos == end) {
+      clearInterval(id);
+      document.getElementById("winner").src = "sailormars.png";
+    } else {
+      pos++;
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+    function myMove4() {
+  var elem = document.getElementById("animate4");
+  var pos = 0;
+  var id = setInterval(frame, Math.floor((Math.random() * 8) + 1));
+  var end = (window.innerWidth - 100);
+  function frame() {
+    if (pos == end) {
+      clearInterval(id);
+      document.getElementById("winner").src = "sailormercury.png";
+    } else {
+      pos++;
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+    function myMove5() {
+  var elem = document.getElementById("animate5");
+  var pos = 0;
+  var id = setInterval(frame, Math.floor((Math.random() * 12) + 1));
+  var end = (window.innerWidth - 100);
+  function frame() {
+    if (pos == end) {
+      clearInterval(id);
+      document.getElementById("winner").src = "sailorvenus.png";
+    } else {
+      pos++;
+      elem.style.left = pos + 'px';
+    }
+  }
 }
